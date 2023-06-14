e84f679e85dec53e81c7b9157488251d78baf799
Sangeethaposam <120437076+Sangeethaposam@users.noreply.github.com>
2023-06-14 19:06:27 +0530

    [Sangeetha] Refactor : Register the user and performing login user, here i performed bycrypt.

--- src/services/user.service.js
+++ src/services/user.service.js
@@ -1,4 +1,5 @@
 import User from '../models/user.model';
+import bcrypt from "bcrypt";
 
 //get all users
 export const getAllUsers = async () => {
@@ -7,18 +8,33 @@ export const getAllUsers = async () => {
 };
 
 //create new user
-export const newUser = async (body) => {
+export const registerUser = async (body) => {
+const email = await User.findOne({email : body.email});
+if(email){
+  throw new Error("Email already exists");
+}
+//const saltRounds = 10;
+else{
+const hash = bcrypt.hashSync(body.password, 10);
+  body.password = hash;
   const data = await User.create(body);
   return data;
+}
 };
 
 //login user
 export const loginUser = async (body) => {
-  const data = await User.findOne({email : body.email,password : body.password});
-  if(data){
-  return data;
+  const data = await User.findOne({email : body.email});
+  if(!data){
+    throw new Error("invalid email");
+  }else {
+    if(bcrypt.compareSync(body.password,data.password)){
+       return data;
   }else{
-    throw new Error("Invalid details");
+    throw new Error("Invalid password");
   }
+ }
 };
+
+
 
