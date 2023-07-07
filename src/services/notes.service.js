47bb2e50ed175921607b649490a5fe1fb00422df
Sangeethaposam <120437076+Sangeethaposam@users.noreply.github.com>
2023-07-07 16:56:19 +0530

    [Sangeetha] Refactor :Here i have the implementation of all functions, Here i am handling the errors

--- src/services/notes.service.js
+++ src/services/notes.service.js
@@ -9,49 +9,55 @@ export const createNote = async (body) => {
       if(data){
         await client.del(body.createdBy);
       return data;
+      }else{
+        throw new Error("Note creation failed");
       }
-      return data;
     };
 
 //get all notes
 export const getAllNotes = async (body) => {
   const  limit  = 5;
   const data = await Note.find({createdBy: body.createdBy}).limit(parseInt(limit));
-  if(data){
+  if(!data){
+  throw new Error("Fetching all notes failed");
+  } else{
   await client.set(body.createdBy,JSON.stringify(data));
-  return data;
-  }
   const dataProduce = JSON.stringify(data);
   rabbit.producer('GetNotes',dataProduce);
   return data;
+  }
 };
 //get single note
 export const getNote = async (_id,body) => {
   const data = await Note.findById({_id: _id, createdBy: body.createdBy});
   await client.set(_id,JSON.stringify(data));
+  if(!data){
+    throw new Error("note is not present based on this id");
+  }else{
   return data;
+  }
 };
 
 //update single note
 export const updateNotes = async (_id,createdBy, body) => {
-  const data = await Note.findByIdAndUpdate(
-    {
-      _id: _id
-    ,
-      createdBy: createdBy
-    },
-    body,
-    {
-      new: true
-    }
-  );
+  const data = await Note.findByIdAndUpdate({_id: _id,createdBy: createdBy},body,{new: true});
+  if(!data){
+    throw new Error("Updated failed provide valid id");
+  }else{
+    await client.set(_id,JSON.stringify(data));
   return data;
+  }
 };
 
 //delete note
 export const deleteNote = async (_id,body) => {
-  await Note.findByIdAndDelete({_id},{createdBy: body.createdBy});
+  const data =  await Note.findByIdAndDelete({_id},{createdBy: body.createdBy});
+  if(!data){
+    throw new Error("Updated failed provide valid id");
+  }else{
+  client.del(_id);
   return '';
+  }
 };
 
 export const addRemoveTrash = async (_id) => {
@@ -104,6 +110,8 @@ export const countNotes = async (body) => {
      countData['getallcount'] = await Note.countDocuments({createdBy: body.createdBy});
     
     return countData;
+  }else{
+    throw new Error("User Not Found");
   }
 
 };
